11a0e5a359dbf18110b1160b7379c9433560a6ca
QuasimKhan <quasimkhanpersonal@gmail.com>
2025-12-22 19:11:21 +0530

    4 questions solved

---
+++ array.js
@@ -0,0 +1,56 @@
+// // Q - find the largest number in an array
+
+// const arr = [10, 20, 12, 32, 100, 110, 20, 30, 1];
+
+// let largest = arr[0];
+
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > largest) {
+//         largest = arr[i];
+//     }
+// }
+
+// console.log("Largest Number - ", largest);
+
+//Q - Find the smallest number in an array
+
+// const arr1 = [10, 20, 3, 34, 2, 12, 3123, 3342, 432442, 11, 2];
+
+// let smallest = arr1[0];
+
+// for (let i = 1; i < arr.length; i++) {
+//     if (smallest > arr1[i]) {
+//         smallest = arr1[i];
+//     }
+// }
+
+// console.log("Smallest Number - ", smallest);
+
+// // Q - Count how many numbers are even
+
+// const arr2 = [10, 20, 2, 3, 4, 5, 3, 6];
+
+// let evenCount = 0;
+
+// for (let i = 0; i < arr2.length; i++) {
+//     if (arr2[i] % 2 === 0) {
+//         evenCount++;
+//     }
+// }
+
+// // console.log("Total Even Number - ", evenCount);
+
+// // Q - check if the array is sorted
+
+// const arr = [1, 2, 3, 4, 5];
+
+// let isArraySorted = true;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > arr[i + 1]) {
+//         isArraySorted = false;
+//         break;
+//     }
+// }
+
+// console.log("Is array sorted - ", isArraySorted);
