a250457f12ce1d3e40e290f3bdd6d8d89913d6a9
QuasimKhan <quasimkhanpersonal@gmail.com>
2025-12-25 16:16:09 +0530

    add isPalindrome and isValidPalindrome functions with example usage

--- strings.js
+++ strings.js
@@ -13,3 +13,52 @@
 // }
 
 // console.log("Total vowel - ", countVowel("aeiou"));
+
+// // Q - Is strign palidrome
+
+// function isPalindrome(str) {
+//     let left = 0;
+//     let right = str.length - 1;
+
+//     while (left < right) {
+//         if (str[left] !== str[right]) {
+//             return false;
+//         }
+//         left++;
+//         right--;
+//     }
+//     return true;
+// }
+
+// console.log("Is palindrome - ", isPalindrome("agqga"));
+
+// Q - Is valid palindDrome
+
+// function isValidPalindrome(str) {
+//     let newStr = "";
+//     const validAlphaNumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
+
+//     for (let char of str.toLowerCase()) {
+//         if (validAlphaNumerical.includes(char)) {
+//             newStr += char;
+//         }
+//     }
+
+//     console.log("new string", newStr);
+
+//     let left = 0;
+//     let right = newStr.length - 1;
+
+//     while (left < right) {
+//         if (newStr[left] !== newStr[right]) {
+//             return false;
+//         }
+
+//         left++;
+//         right--;
+//     }
+
+//     return true;
+// }
+
+// console.log(isValidPalindrome("agq ga"));
