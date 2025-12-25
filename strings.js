ad59002389e8f5db97d410b0c8dd7618ceb2d54b
QuasimKhan <quasimkhanpersonal@gmail.com>
2025-12-25 15:10:33 +0530

    implement function to count total vowels in a string

---
+++ strings.js
@@ -0,0 +1,15 @@
+// // Q - Find the total vowel present in a string
+
+// function countVowel(str) {
+//     let count = 0;
+//     const vowels = "aeiou";
+
+//     for (let char of str.toLowerCase()) {
+//         if (vowels.includes(char)) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// console.log("Total vowel - ", countVowel("aeiou"));
