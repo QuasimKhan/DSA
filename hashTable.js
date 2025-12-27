928956b5309fbe2121bf7107433dfaf34e2077a1
QuasimKhan <quasimkhanpersonal@gmail.com>
2025-12-27 18:53:58 +0530

    comment out firstRepeating function and its usage for future reference

--- hashTable.js
+++ hashTable.js
@@ -1,22 +1,22 @@
-// Q - Find first repeating element of an Array
+// // Q - Find first repeating element of an Array
 
-function firstRepeating(arr) {
-    let freq = {};
-    let firstRepeatingElement = -1;
+// function firstRepeating(arr) {
+//     let freq = {};
+//     let firstRepeatingElement = -1;
 
-    for (let i = 0; i < arr.length; i++) {
-        let element = arr[i];
+//     for (let i = 0; i < arr.length; i++) {
+//         let element = arr[i];
 
-        if (freq[element]) {
-            freq[element]++;
-            firstRepeatingElement = element;
-            break;
-        } else {
-            freq[element] = 1;
-        }
-    }
+//         if (freq[element]) {
+//             freq[element]++;
+//             firstRepeatingElement = element;
+//             break;
+//         } else {
+//             freq[element] = 1;
+//         }
+//     }
 
-    return firstRepeatingElement;
-}
+//     return firstRepeatingElement;
+// }
 
-console.log(firstRepeating([1, 2, 3, 3, 4, 5]));
+// console.log(firstRepeating([1, 2, 3, 3, 4, 5]));
